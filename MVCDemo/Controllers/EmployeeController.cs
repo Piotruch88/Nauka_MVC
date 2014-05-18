4c52248edbfc842a04252696a969b03860ece760
Piotruch88 <piotruch88@gamail.com>
2014-05-18 16:21:24 +0200

    Comit dla bezpieczeństwa

--- MVCDemo/Controllers/EmployeeController.cs
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -9,10 +9,17 @@ namespace MVCDemo.Controllers
 {
     public class EmployeeController : Controller
     {
+        public ActionResult Index()
+        {
+            EmployeeContext employeeContext = new EmployeeContext();
+            List<Employee> employees = employeeContext.Employees.ToList();
+            return View(employees);
+        }
+
         public ActionResult Details(int id)
         {
             EmployeeContext employeeContext = new EmployeeContext();
-            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeiD == id);
+            Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
             
             return View(employee);
         }
--- MVCDemo/Models/Employee.cs
+++ MVCDemo/Models/Employee.cs
@@ -9,10 +9,9 @@ namespace MVCDemo.Models
     [Table("tblEmployee")]
     public class Employee
     {
-        public int EmployeeiD { get; set; }
+        public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
-        public string City { get; set; }
-        public int DepartmentId { get; set; }
+        public string City { get; set; }        
     }
 }
