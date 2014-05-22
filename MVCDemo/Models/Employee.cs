a2434cf1c03161543c7ddf4e91488a525f00b095
Piotruch88 <piotruch88@gamail.com>
2014-05-22 20:47:36 +0200

    Całośc PART 10 :D COCO JUMBO I DO PRZODU

--- MVCDemo/Models/Employee.cs
+++ MVCDemo/Models/Employee.cs
@@ -12,6 +12,7 @@ namespace MVCDemo.Models
         public int EmployeeId { get; set; }
         public string Name { get; set; }
         public string Gender { get; set; }
-        public string City { get; set; }        
+        public string City { get; set; }
+        public int DepartmentId { get; set; }
     }
 }
